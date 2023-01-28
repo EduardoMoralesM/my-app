f5e8793bef8b0ed7b7a2157bd27235f94c7daba1
Turism099@ <lalomrales98@gmail.com>
2023-01-27 22:13:15 -0600

    My-app

---
+++ src/main/java/com/eduardomm/myapp/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.eduardomm.myapp.entity;
+
+/**
+ * @author eduardo
+ *
+ */
+public class TestEntity {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
